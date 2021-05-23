2298687ccf97f3407b4f288fc355c78c6bc7a160
aymenJeddey <aymen.jeddey@esprit.tn>
2021-05-23 20:17:05 +0100

    ok

--- src/test/java/tn/esprit/spring/services/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/EmployeServiceImplTest.java
@@ -1,16 +1,12 @@
 package tn.esprit.spring.services;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 public class EmployeServiceImplTest {
 
-	EmployeServiceImpl iemployeservice = new EmployeServiceImpl();
-
-	@Test
-	public void testEmployeWithoutTimesheet() {
-		//assertEquals(0, iemployeservice.getListEmployesWithoutTimesheet());
-	}
+//	EmployeServiceImpl iemployeservice = new EmployeServiceImpl();
+//
+//	@Test
+//	public void testEmployeWithoutTimesheet() {
+//		//assertEquals(0, iemployeservice.getListEmployesWithoutTimesheet());
+//	}
 
 }
