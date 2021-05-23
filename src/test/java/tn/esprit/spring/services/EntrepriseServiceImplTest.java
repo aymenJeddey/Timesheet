62d37b331f6a50050e5b73f75444d9bfbc8d8016
aymenJeddey <aymen.jeddey@esprit.tn>
2021-05-23 12:37:47 +0100

    Update EntrepriseServiceImplTest.java

--- src/test/java/tn/esprit/spring/services/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/EntrepriseServiceImplTest.java
@@ -1,5 +1,17 @@
 package tn.esprit.spring.services;
 
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
 public class EntrepriseServiceImplTest {
+
+	
+	EntrepriseServiceImpl entrepriseServiceImpl = new EntrepriseServiceImpl();
+
+	@Test
+	public void testCalculerSom() {
+	assertNotEquals(0, entrepriseServiceImpl.calculNombreDepartementByEntreprise(0));
+	}
 
 }
